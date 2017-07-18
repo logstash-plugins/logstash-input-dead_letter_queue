18e11d25d70e4e81a5560226b214a1b981636a30
Rob Bavey <rob.bavey@elastic.co>
2017-07-18 13:38:28 -0400

    Change jruby compile dependency to be jruby 9k
    Also increases test sleep to attempt to fix travis test failure
    
    Version bump to 1.0.5

--- src/test/java/org/logstash/input/DeadLetterQueueInputPluginTests.java
+++ src/test/java/org/logstash/input/DeadLetterQueueInputPluginTests.java
@@ -66,7 +66,7 @@ public class DeadLetterQueueInputPluginTests {
             }
         });
         pluginThread.start();
-        Thread.sleep(2000);
+        Thread.sleep(15000);
         assertEquals(10000, count.get());
         queueWriter.writeEntry(entry);
         Thread.sleep(200);
