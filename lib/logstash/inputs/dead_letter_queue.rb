47fcaaec088893a51d8f068a7d074ee7b3f4e6b7
Rodrigo López Dato <rdlp@amazon.com>
2019-05-02 16:50:26 +0100

    Preserve @metadata when deserialising Java events
    See https://github.com/elastic/logstash/pull/10749

--- lib/logstash/inputs/dead_letter_queue.rb
+++ lib/logstash/inputs/dead_letter_queue.rb
@@ -58,7 +58,7 @@ class LogStash::Inputs::DeadLetterQueue < LogStash::Inputs::Base
   public
   def run(logstash_queue)
     @inner_plugin.run do |entry|
-      event = LogStash::Event.new(entry.event.toMap())
+      event = LogStash::Event.new(entry.event.clone())
       event.set("[@metadata][dead_letter_queue][plugin_type]", entry.plugin_type)
       event.set("[@metadata][dead_letter_queue][plugin_id]", entry.plugin_id)
       event.set("[@metadata][dead_letter_queue][reason]", entry.reason)
